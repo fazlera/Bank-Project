4c3b069b7fe6894ee82aa57b0721b606c5410bb2
FAZLE RABBI <mdfazlerabbim621@gmail.com>
2024-09-01 19:45:08 +0600

    updated balance

--- jsFile/withdraw.js
+++ jsFile/withdraw.js
@@ -5,23 +5,25 @@ document.getElementById('withdraw-btn').addEventListener('click',function(){
     const newWithdrawAmountString = withdrawInputField.value;
     const newWithdrawAmount = parseFloat(newWithdrawAmountString)
     
-
     const withdrawTotalValue = document.getElementById('withdraw-total')
     const previousWithdrawAmountString = withdrawTotalValue.innerText;
     const previousWithdrawAmount = parseFloat(previousWithdrawAmountString)
-
     const currentWithDraw = previousWithdrawAmount + newWithdrawAmount;
-
-    withdrawTotalValue.innerText = currentWithDraw;
 
     const balanceTotal = document.getElementById('balance-total')
     const previousBalanceTotalString = balanceTotal.innerText
     const newBalanceTotal = parseFloat(previousBalanceTotalString)
-    
+    if(newBalanceTotal == 0){
+        return alert('not balance')
+     }
 
     const withdrawCurrentBalance = newBalanceTotal - newWithdrawAmount  ;
-
     balanceTotal.innerText = withdrawCurrentBalance;
+
+    
+    
+
+    withdrawTotalValue.innerText = currentWithDraw;
 
     withdrawInputField.value = '';
 
